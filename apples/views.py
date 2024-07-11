ebd9e698dfa4f460824272a3433f271735181049
Nelson Bermeo <141251675+Melson12@users.noreply.github.com>
2024-07-11 07:45:50 -0400

    serializers, urls, views changed

--- apples/views.py
+++ apples/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from .models import Apple
+from .serializers import serialize_apples
+from django.http import JsonResponse
 
-# Create your views here.
+
+def apple_list(request) -> JsonResponse:
+    apples = Apple.objects.all()
+    return JsonResponse(serialize_apples(apples), safe=False)
